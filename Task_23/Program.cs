fc02cb79772f4fd863943b835adf8e0e36a7d384
Darya <darecc@yandex.ru>
2022-08-26 21:57:56 +0400

    add Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int count = 1; count<=N; count++) {
+	double result = Math.Pow(count, 3);
+	Console.WriteLine(result);
+}
