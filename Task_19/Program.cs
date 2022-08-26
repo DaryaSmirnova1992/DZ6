ac49ac654ee2aaab345cd94850680ff2899854d7
Darya <darecc@yandex.ru>
2022-08-26 20:16:24 +0400

    add Task 19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("Введите пятизначное число: ");
+ int num1 = int.Parse(Console.ReadLine());
+ int f1 = num1 / 10000;
+ int f2 = (num1 - f1 * 10000) / 1000;
+ int r1 = num1 % 10;
+ int r2 = (num1 % 100) / 10;
+ if(10000 > num1 || num1 > 99999)
+ {
+    Console.Write("Введено не пятизначное число");
+ }
+    else if(f1 == r1 && f2 == r2)
+    {
+        Console.Write("Палиндром");
+    }
+    else
+    {
+        Console.Write("Не палиндром");
+    }
